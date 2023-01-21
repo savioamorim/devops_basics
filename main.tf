2dde78751df91b491d4aaa6178c648a2c50d3b7d
Savio Amorim <savioamorim.s@hotmail.com>
2023-01-21 10:30:10 -0300

    Ansible config

--- main.tf
+++ main.tf
@@ -17,6 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = "ami-095413544ce52437d"
   instance_type = "t2.micro"
   key_name = "file-name"
+  # user_data = <<-EOF
+  #                #!/bin/bash
+  #                cd /home/ubuntu
+  #                echo "<h1>Hello World</h1>" > index.html
+  #                nohup busybox httpd -f- -p 8080 &
+  #                EOF
   tags = {
     Name = "Hello World"
   }
